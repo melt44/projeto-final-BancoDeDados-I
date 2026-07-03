455bff42436c21496a18299108f2d584029155c9
msiqueiras <devef.msiqueira@gmail.com>
2026-07-03 01:11:28 -0300

    feat: criação da tabela 'itempedido'

---
+++ migrations/008_itemproduto.sql
@@ -0,0 +1,37 @@
+CREATE TABLE public.itemproduto(
+    id_itempedido uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    pedido_id uuid NOT NULL,
+    produto_id uuid NOT NULL,
+    estoque_id uuid NOT NULL,
+    quantidade INTEGER NOT NULL,
+    preco_unitario NUMERIC(6,2) NOT NULL,
+
+    CONSTRAINT uq_pedido_produto
+        UNIQUE (pedido_id, produto_id),
+
+    CONSTRAINT fk_itemproduto_pedido
+        FOREIGN KEY (pedido_id)
+        REFERENCES pedidos(id_pedido)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_itemproduto_produto
+        FOREIGN KEY (produto_id)
+        REFERENCES produtos(id_produto)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_itemproduto_estoque
+        FOREIGN KEY (estoque_id)
+        REFERENCES estoques(id_estoque)
+        ON DELETE CASCADE,
+
+    CONSTRAINT validar_quantidade CHECK(
+        quantidade > 0
+    ),
+
+    CONSTRAINT validar_preco CHECK(
+        preco_unitario > 0 AND
+        preco_unitario < 9999.99
+    )
+
+
+);
